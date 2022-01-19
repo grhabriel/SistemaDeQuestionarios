80b6e889b254a508e9ee43eff8589bda563f3443
grhabriel <ghx.barbosahg@gmail.com>
2022-01-18 21:53:45 -0300

    Uma pergunta por vez

--- public/proximaPergunta.js
+++ public/proximaPergunta.js
@@ -1,16 +1,41 @@
 let quantidadePerguntas = document.querySelector("#quantidadePerguntas")
-let botaoProximo = document.querySelector(".botaoProx");
-
 let respostasSubmetidas = [];
-let contadorPerguntaAtual =  1;
+let contadorPerguntaAtual =  0;
+
+let container = document.querySelector("main");
 
 
+let primeiraPergunta = document.querySelector("#pergunta0");
+primeiraPergunta.classList.remove("perguntaEscondida");
+primeiraPergunta.classList.add("perguntaMostrar");
 
-botaoProximo.addEventListener("click", ()=>{
-    let radios = document.getElementsByName(`respostaPergunta${contadorPerguntaAtual}`);
-    for(radio of radios){
-        console.log(radio.checked);
-        console.log("Valor: "+radio.value);
+function proximaPergunta(e){
+    let input = document.querySelector(`input[name="respostaPergunta${contadorPerguntaAtual}"]:checked`);
+    respostasSubmetidas.push(input.value);
+    for(let i = 0; i<2; i++){
+        container.removeChild(container.firstChild);
     }
+   
+    contadorPerguntaAtual++;
+    if(contadorPerguntaAtual >= quantidadePerguntas-1){
+        console.log("a");
+        return null;
+    }
+    let novaPergunta = document.querySelector(`#pergunta${contadorPerguntaAtual}`);
+    if(!novaPergunta){
+        return null;
+    }
+    novaPergunta.classList.add("perguntaMostrar");
+  
+    
 
-});
+    novaPergunta.classList.add("perguntaMostrar");
+    console.log(respostasSubmetidas);
+    
+}
+
+let botoesProximo = document.getElementsByClassName("botaoProx");
+for(botao of botoesProximo){
+    botao.addEventListener("click",proximaPergunta);
+}
+
