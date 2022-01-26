9a4967a0a8cf00434b563a6f84a9bf70b2d3c834
Nathan Filipe <86198242+nathanfl1@users.noreply.github.com>
2022-01-26 08:04:54 -0300

    Trabalho Pratico 2 3.8

---
+++ public/botoes.js
@@ -0,0 +1,36 @@
+
+let botaoJogar = document.querySelector("#jogar");
+
+botaoJogar.addEventListener('click', function () {
+    window.location = "/questionarios";
+});
+
+let botaoCriar = document.querySelector("#criar");
+
+botaoCriar.addEventListener('click', function () {
+    window.location = "/questionarios/adicionar";
+});
+
+let botaoParticipantes = document.querySelector("#participantes");
+botaoParticipantes.addEventListener('click', abreModal);
+
+function abreModal(e) {
+    let janelaModalEl = document.querySelector('.modal');
+    janelaModalEl.classList.add('visivel');
+}
+
+let botoesFecharModal = document.querySelectorAll('.fecharModal');
+
+for (let fecharEl of botoesFecharModal) {
+    fecharEl.addEventListener('click', fechaModal);
+}
+
+function fechaModal(e) {
+    let janelaModalEl = document.querySelector('.modal');
+    janelaModalEl.classList.remove('visivel');
+}
+
+let divCadastro = document.querySelector("#cadastro");
+
+
+
--- public/webStorage.js
+++ public/webStorage.js
@@ -2,24 +2,29 @@ let botaoInscreverEl = document.querySelector('#cadastrar');
 let botaoLoginEl = document.querySelector('#login');
 let divCadastro = document.querySelector("#cadastro");
 let botaoManterConectado = document.querySelector('#manterConectado');
+let divPrincipal = document.querySelector("#quadroInicial");
 
 if (localStorage.getItem("manter-conectado") === "true")
     divCadastro.style.display = 'none';
 else {
+    if (divCadastro.style.display != "none") {
+        divPrincipal.style.display = "none";
+    }
     botaoInscreverEl.addEventListener('click', inscricao);
     function inscricao() {
         let nomeUsuarioEl = document.querySelector('#nome');
         let nomeUsuario = nomeUsuarioEl.value;
         if (nomeUsuario) {
-            localStorage.setItem('nome', JSON.stringify(nomeUsuario));
+            localStorage.setItem('nome', nomeUsuario);
             alert("Inscrição feita com sucesso!");
 
             if (botaoManterConectado.checked === true)
                 localStorage.setItem("manter-conectado", "true");
             else
                 localStorage.setItem("manter-conectado", "false");
-            
+
             divCadastro.style.display = 'none';
+            divPrincipal.style.display = 'block';
         }
         else
             alert("Digite seu nome pra continuar!");
@@ -32,20 +37,26 @@ else {
 
         if (nomeUsuario) {
             let nomeLocal = localStorage.getItem('nome');
-            nomeLocal = JSON.parse(nomeLocal);
 
             if (nomeLocal == nomeUsuario) {
                 window.alert('Login realizado com sucesso!');
-                if (botaoManterConectado.checked === "true")
+                if (botaoManterConectado.checked === true)
                     localStorage.setItem("manter-conectado", "true");
                 else
                     localStorage.setItem("manter-conectado", "false");
                 divCadastro.style.display = "none";
+                divPrincipal.style.display = "block";
             }
             else
-                window.alert('erro');
+                window.alert('Usuário inválido, tente novamente!');
         }
         else
             alert("Digite seu nome pra continuar!");
     }
 }
+let botaoDeslogar = document.querySelector('#deslogar');
+botaoDeslogar.addEventListener('click', function () {
+    localStorage.setItem('manter-conectado', 'false');
+    divCadastro.style.display = 'block';
+    divPrincipal.style.display = 'none';
+});
